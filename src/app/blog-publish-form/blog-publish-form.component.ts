917e356618b32c367ab0b559b9bc74d137baca26
limoiie <limo.iie4@gmail.com>
2020-02-22 12:12:19 +0800

    refactor: Adjust publish form

--- src/app/blog-publish-form/blog-publish-form.component.ts
+++ src/app/blog-publish-form/blog-publish-form.component.ts
@@ -1,11 +1,6 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {BlogService} from '../blog.service';
-import {MatChipInputEvent} from '@angular/material/chips';
-import {MatAutocomplete, MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {Observable} from 'rxjs';
-import {map, startWith, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-blog-publish-form',
@@ -15,9 +10,6 @@ import {map, startWith, tap} from 'rxjs/operators';
 export class BlogPublishFormComponent implements OnInit {
 
   folders$;
-  allTags$;
-
-  allTags: string[] = [];
 
   profileForm = new FormGroup({
     title: new FormControl(''),
@@ -29,76 +21,18 @@ export class BlogPublishFormComponent implements OnInit {
     abstract: new FormControl('')
   });
 
-  tagCtrl = this.profileForm.get('tags');
-
-  filteredTags: Observable<string[]>;
-
   @Input() title = '';
   @Input() author = '';
   @Input() abstract = '';
   tags: string[] = [];
 
-  // @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
-  // @ViewChild('auto') matAutocomplete: MatAutocomplete;
-
   constructor(
     private blogService: BlogService
   ) {
     this.folders$ = this.blogService.loadFolders();
-    // this.allTags$ = this.blogService.loadTags().pipe(
-    //   tap((tags: any) => this.allTags = tags)
-    // );
-    // this.filteredTags = this.tagCtrl.valueChanges.pipe(
-    //     startWith(null),
-    //     map((fruit: string | null) => fruit ?
-    //       this._filter(fruit) : this.tags.slice()));
   }
 
   ngOnInit(): void {
-  }
-
-  // add(event: MatChipInputEvent): void {
-  //   const input = event.input;
-  //   const value = event.value.toLowerCase();
-  //
-  //   // Add our fruit
-  //   if ((value || '').trim()) {
-  //     if (this.tags.indexOf(value.trim()) === -1) {
-  //       this.tags.push(value.trim());
-  //     }
-  //   }
-  //
-  //   // Reset the input value
-  //   if (input) {
-  //     input.value = '';
-  //   }
-  //
-  //   this.tagCtrl.setValue(null);
-  // }
-  //
-  // remove(tag: string): void {
-  //   const value = tag.toLowerCase();
-  //   const index = this.tags.indexOf(value);
-  //
-  //   if (index >= 0) {
-  //     this.tags.splice(index, 1);
-  //   }
-  // }
-  //
-  // selected(event: MatAutocompleteSelectedEvent): void {
-  //   const value = event.option.viewValue.toLowerCase();
-  //   if (this.tags.indexOf(value) === -1) {
-  //     this.tags.push(value);
-  //   }
-  //   this.fruitInput.nativeElement.value = '';
-  //   this.tagCtrl.setValue(null);
-  // }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.allTags.filter(fruit =>
-      fruit.toLowerCase().indexOf(filterValue) === 0);
   }
 
   onSubmit() {
